a441296693114afe0ac9b1dea4a42604ea2a6cf6
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2023-07-02 11:36:24 +0100

    major config

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function Landing() {
+  return (
+    <Box className="flex h-full items-center justify-center">
+      <Typography>Money</Typography>
+    </Box>
+  );
+}

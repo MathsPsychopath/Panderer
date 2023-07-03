4794f47b8a800a152cd4025c369ee53cf3f2a3aa
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2023-07-03 02:13:13 +0100

    added assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#030a0c",
+        background: "#ffffff",
+        "primary-button": "#3663c4",
+        "secondary-button": "#f8f7fd",
+        accent: "#2f2074",
+      },
+    },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
 
